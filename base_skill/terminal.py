933ab0394908bafe041d2c7129c84d16a306692b
Yan Zhang <zhangyan612@gmail.com>
2017-11-02 18:00:01 -0500

    update on folder structure

--- base_skill/terminal.py
+++ base_skill/terminal.py
@@ -1,3 +1,16 @@
 # input text to terminal
 
 
+# shell script
+
+def execute(command):
+    cmd = subprocess.Popen(command, stdout=subprocess.PIPE)
+    output = cmd.communicate()[0]
+    return output
+
+
+
+import subprocess
+test = subprocess.Popen(["ping","-W","2","-c", "1", "192.168.1.70"], stdout=subprocess.PIPE)
+output = test.communicate()[0]
+print(output)
