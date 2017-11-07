dcb9eebac75b09ab7fb1ad4d1181068c7b023f31
Yan Zhang <zhangyan612@gmail.com>
2017-11-07 15:22:50 -0600

    fix move

--- robotics/basic_control.py
+++ robotics/basic_control.py
@@ -7,15 +7,15 @@ print("Press:\n\tw: Move Robot forward\n\ta: Turn Robot left\n\td: Turn Robot ri
 while True:
 	a=input("Enter the Command:")	# Fetch the input from the terminal
 	if a=='w':
-		forward(1)	# Move forward
+		move.forward(1)	# Move forward
 	elif a=='a':
-		left(1)	# Turn left
+		move.left(1)	# Turn left
 	elif a=='d':
-		right(1)	# Turn Right
+		move.right(1)	# Turn Right
 	elif a=='s':
-		reverse(1)	# Move back
+		move.reverse(1)	# Move back
 	elif a=='x':
-		stop()	# Stop
+		move.stop()	# Stop
 	# elif a=='t':
 	# 	increase_speed()	# Increase speed
 	# elif a=='g':
