144838dd9cef88bec078a0d753b91a1a02c193b7
Yan Zhang <zhangyan612@gmail.com>
2017-11-06 23:22:40 -0600

    indentation

--- robotics/move.py
+++ robotics/move.py
@@ -26,32 +26,31 @@ def forward(tf):
     time.sleep(tf)
     gpio.cleanup()
 
- def left(tf):
-    init()
-    gpio.output(17, True)
-    gpio.output(22, True)
-    gpio.output(23, False)
-    gpio.output(24, False)
-    time.sleep(tf)
-    gpio.cleanup()
+#  def left(tf):
+#     init()
+#     gpio.output(17, True)
+#     gpio.output(22, True)
+#     gpio.output(23, False)
+#     gpio.output(24, False)
+#     time.sleep(tf)
+#     gpio.cleanup()
 
- def right(tf):
-    init()
-    gpio.output(17, False)
-    gpio.output(22, False)
-    gpio.output(23, True)
-    gpio.output(24, True)
-    time.sleep(tf)
-    gpio.cleanup()
+#  def right(tf):
+#     init()
+#     gpio.output(17, False)
+#     gpio.output(22, False)
+#     gpio.output(23, True)
+#     gpio.output(24, True)
+#     time.sleep(tf)
+#     gpio.cleanup()
 
-
- def stop():
-    gpio.output(Motor1E,gpio.LOW)
-    gpio.output(Motor2E,gpio.LOW)
+#  def stop():
+#     gpio.output(Motor1E,gpio.LOW)
+#     gpio.output(Motor2E,gpio.LOW)
 
 
 forward(1)
 reverse(1)
-left(1)
-right(1)
-stop()
+# left(1)
+# right(1)
+# stop()
