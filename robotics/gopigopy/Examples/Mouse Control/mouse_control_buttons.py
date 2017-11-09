30fa968608f3d7a63cd9b30ef8e029e9f9f6d3c4
Yan Zhang <zhangyan612@gmail.com>
2017-11-09 11:56:49 -0600

    test mouse control

--- robotics/gopigopy/Examples/Mouse Control/mouse_control_buttons.py
+++ robotics/gopigopy/Examples/Mouse Control/mouse_control_buttons.py
@@ -1,19 +1,7 @@
 #!/usr/bin/env python
-########################################################################                                                                  
-# This example is for controlling the GoPiGo robot from a mouse buttons                        
-# http://www.dexterindustries.com/GoPiGo/                                                                
-# History
-# ------------------------------------------------
-# Author     Date      		Comments
-# Karan      13 June 14 	Initial Authoring
-# 			                                                         
-# These files have been made available online through a Creative Commons Attribution-ShareAlike 3.0  license.
-# (http://creativecommons.org/licenses/by-sa/3.0/)           
-#
-########################################################################
 import struct
 import sys
-from gopigo import *
+# from gopigo import *
 
 #Open the stream of data coming from the mouse
 file = open( "/dev/input/mice", "rb" );
@@ -41,23 +29,24 @@ def getMouseEvent():
 flag=0
 print "Press Enter to start"
 a=raw_input()	#Wait for an input to start
-set_speed(speed)
-stop()
+# set_speed(speed)
+# stop()
 while( 1 ):
 	[l,m,r,x,y]=getMouseEvent()	#Get the inputs from the mouse
 	if debug:
 		print l,m,r,x,y
 		
 	if flag==1: #If left or right mouse not pressed, move forward
-		fwd()
+		# fwd()
 		flag=0
 	if l:		#If left mouse buton pressed, turn left
-		left()
+		# left()
 		flag=1
 	if m:		#If middle mouse button pressed, stop
-		stop()
+		# stop()
 	if r:		#If right mouse button presses, turn right
-		right()
+		# right()
 		flag=1
 	if l and r:	#If both the left and right mouse buttons pressed, go back
-		bwd()
+		# bwd()
+		print('go back')
