b71c420b68e7e0a4b8b4da6bbd9f50f13536f559
Yan Zhang <zhangyan612@gmail.com>
2017-11-19 16:16:20 -0600

    double motor

--- robotics/motor_test.py
+++ robotics/motor_test.py
@@ -1,13 +1,17 @@
 from gpiozero import PWMOutputDevice
 from time import sleep
 
-pwm = PWMOutputDevice(4)
+left_motor = PWMOutputDevice(4)
+right_motor = PWMOutputDevice(17)
+
 while True:
-  pwm.on()
+  left_motor.on()
+  right_motor.on()
   sleep(1)
-  pwm.off()
-  sleep(1)
-  pwm.value = 0.5
-  sleep(1)
-  pwm.value = 0.0
-  sleep(1)
+  left_motor.off()
+  right_motor.off()
+#   sleep(1)
+#   pwm.value = 0.5
+#   sleep(1)
+#   pwm.value = 0.0
+#   sleep(1)
