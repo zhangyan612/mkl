b5279f6064f1f5850aa199cb99c1b50c8e48c9e3
Yan Zhang <zhangyan612@gmail.com>
2017-11-22 23:59:07 -0600

    add library

--- robotics/serial_test/publisher.py
+++ robotics/serial_test/publisher.py
@@ -1,7 +1,15 @@
-import serial 
-import time
-SerialData = serial.Serial('/dev/ttyACM0',9600)
+# import serial 
+# import time
+# SerialData = serial.Serial('/dev/ttyACM0',9600)
 
-while True:
-    SerialData.write('1')
-    time.sleep(5)
+# while True:
+#     SerialData.write('1')
+#     time.sleep(5)
+
+
+
+import arduinoserial
+
+arduino = arduinoserial.SerialPort('/dev/ttyACM0', 9600)
+print arduino.read_until('\n')
+arduino.write('a5050')
