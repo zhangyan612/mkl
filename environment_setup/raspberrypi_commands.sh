61b9911055ae7a35bd0721328e46a4dd2bc3e7ee
Yan Zhang <zhangyan612@gmail.com>
2017-12-08 20:05:50 -0600

    update pi

--- environment_setup/raspberrypi_commands.sh
+++ environment_setup/raspberrypi_commands.sh
@@ -6,3 +6,15 @@ sudo su
 raspi-config
 
 sudo shutdown -h now
+
+
+# change password
+# When logged in as the pi user, you can change your password with the  passwd command.
+passwd
+
+# install vnc
+sudo apt-get update
+sudo apt-get install realvnc-vnc-server realvnc-vnc-viewer
+
+sudo raspi-config 
+# Scroll down to VNC and set it to Enabled.
